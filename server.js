78401c2762f3768ea314f3b3f403e23437aa75d3
Rohan-Pise <rohan.pise0110@gmail.com>
2025-09-06 11:52:55 +0530

    Added new Frontend Doman in cors

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 3001;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://note-nest-beige.vercel.app",
   credentials: true
 }));
 
